04ee5d9db141b9537a24f1a95d2ec6c122c89bc5
Anthony Tropeano <anthonytropeano@me.com>
2024-05-28 12:45:24 -0400

    Corrected issue with the fallback resume component not being shown
    on mobile devices that do not support embedded pdfs

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -2,32 +2,33 @@
 
 import { BiSolidError } from "react-icons/bi";
 import { MdCloudDownload } from "react-icons/md";
-import { JSX, Suspense, createRef, useEffect, useState } from "react";
+import { JSX, Suspense, useEffect, useRef, useState } from "react";
 
 const styles = {
-  embed: "rounded-lg shadow-lg h-full w-full max-h-[1125px]",
   object: "w-full h-full flex items-center justify-center",
   downloadDiv:
     "w-full h-full flex flex-col items-center justify-center gap-16 -mt-28",
   h3: "text-center text-2xl md:text-3xl xl:text-4xl font-bold mt-8",
-  p: "p-3 rounded bg-gray-600 text-gray-200 hover:bg-green-600 font-medium flex hover:font-bold drop-shadow-lg ",
+  p: "p-3 rounded bg-gray-600 text-gray-200 hover:bg-green-600 font-medium flex hover:font-bold drop-shadow-lg -mt-12",
   downloadSpan: "flex flex-col items-center justify-center gap-2 pr-2 ",
   cloudIcon: "w-6 h-6",
   errorIcon:
-    "text-red-500 text-8xl sm:text-9xl md:text-10xl lg:text-11xl xl:text-12",
+    "text-red-500 text-8xl sm:text-9xl md:text-10xl lg:text-11xl xl:text-12 mt-8",
 };
+
+function isMobileBrowser() {
+  const userAgent = navigator.userAgent.toLowerCase();
+  return /android|ipad|iphone|ipod/i.test(userAgent);
+}
 
 export const ResumeComponent = (): JSX.Element => {
   const [canShowPdf, setCanShowPdf] = useState(true);
   const [resize, setResize] = useState(false);
-  const pdfRef = createRef<HTMLEmbedElement>();
+  const [mounted, setMounted] = useState(false);
+  const pdfRef = useRef<HTMLEmbedElement | null>(null);
 
   useEffect(() => {
-    const embed = pdfRef.current;
-    // check if the browser supports PDF embedding
-    if (embed && !embed.type.includes("pdf")) {
-      setCanShowPdf(false);
-    }
+    if (!mounted) setMounted(true);
 
     const handleResize = () => {
       setTimeout(() => {
@@ -38,10 +39,13 @@ export const ResumeComponent = (): JSX.Element => {
       }, 0);
     };
 
+    isMobileBrowser() && setCanShowPdf(false);
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return canShowPdf ? (
     <Suspense fallback={<div>Loading...</div>}>
       <object className={styles.object}>
@@ -51,7 +55,6 @@ export const ResumeComponent = (): JSX.Element => {
             aria-label="resume"
             src="./documents/resume.pdf"
             type="application/pdf"
-            className={styles.embed}
           />
         )}
       </object>
